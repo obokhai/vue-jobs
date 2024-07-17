d057019fedaa4166d367ca956c27bc4f83bc2305
obokhai <aibormoses1997@gmail.com>
2024-07-17 10:33:40 +0100

    first commit~

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue'
+import JobsListings from '@/components/JobsListings.vue'
+</script>
+
+<template>
+    <Hero />
+    <HomeCards />
+    <JobsListings :limit="3" :showButton="true"/>
+</template>
